34173be7a602aedc44808d2333938bd2da17448c
IssaAnagreh <isa_anagreh@hotmail.com>
2020-07-02 00:54:39 +0300

    try to add command by server

--- src/main/java/com/worldNavigator/PlayerViewer.java
+++ src/main/java/com/worldNavigator/PlayerViewer.java
@@ -1,15 +1,59 @@
 package com.worldNavigator;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Observable;
 import java.util.Observer;
 
-public class PlayerViewer implements Observer {
+public class PlayerViewer extends HttpServlet implements Observer {
   public final PlayerControllerInterface playerController;
   private String name;
+  private static final long serialVersionUID = 1L;
 
+  /**
+   * @see HttpServlet#HttpServlet()
+   * @param playerController
+   */
   public PlayerViewer(PlayerControllerInterface playerController, String name) {
+    super();
     this.playerController = playerController;
     this.name = name;
     this.playerController.subscribe(this);
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    // Step 1: set content type
+    response.setContentType("text/html");
+
+    // Step 2: get the printwriter
+    PrintWriter out = response.getWriter();
+
+    String command = request.getParameter("command");
+
+    this.playerController.server_command(command);
+    // Step 3: generate the HTML content
+    out.println("<html><body>");
+
+    out.println("The student is confirmed: "
+            + command);
+
+    out.println("</body></html>");
+
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
   }
 
   @Override
