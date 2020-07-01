dc217f25b96c42cdf52134727b6f47561d689b67
IssaAnagreh <isa_anagreh@hotmail.com>
2020-07-02 01:20:17 +0300

    uncomment @WebServlet(/StudentServlet)

--- src/main/java/com/worldNavigator/StudentServlet.java
+++ src/main/java/com/worldNavigator/StudentServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class StudentServlet
  */
-//@WebServlet("/StudentServlet")
+@WebServlet("/StudentServlet")
 public class StudentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
