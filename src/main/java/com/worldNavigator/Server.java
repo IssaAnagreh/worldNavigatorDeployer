5a02a1903f51da1577ea9e5bd1a7d8092bbbfb07
IssaAnagreh <isa_anagreh@hotmail.com>
2020-07-04 00:50:36 +0300

    use methods from server

--- src/main/java/com/worldNavigator/Server.java
+++ src/main/java/com/worldNavigator/Server.java
@@ -12,17 +12,33 @@ public class Server {
 
             System.out.println("Connection is established on port 8080");
 
+            PrintWriter out = new PrintWriter(s.getOutputStream());
+
+            out.println("Player Name is: "
+                    + menu.playerViewer.getName());
+            out.println("<form method=\"GET\">\n" +
+                    "\n" +
+                    "    Type your command: <input type=\"text\" name=\"command\"/>\n" +
+                    "\n" +
+                    "    <br/><br/>\n" +
+                    "\n" +
+                    "    <input type=\"submit\" value=\"Submit\"/>\n" +
+                    "\n" +
+                    "    <p>try4</p>\n" +
+                    "\n" +
+                    "</form>");
+
             DataInputStream inputFromClient = new DataInputStream(
                     s.getInputStream());
             while (true) {
                 // Receive radius from the client
                 String str = (String) inputFromClient.readUTF();
 
-                PrintWriter out = new PrintWriter(s.getOutputStream());
+                out = new PrintWriter(s.getOutputStream());
 
                 out.println("Player Name is: "
                         + menu.playerViewer.getName());
-                out.println("<form action=\"CommandsServer\" method=\"GET\">\n" +
+                out.println("<form method=\"GET\">\n" +
                         "\n" +
                         "    Type your command: <input type=\"text\" name=\"command\"/>\n" +
                         "\n" +
