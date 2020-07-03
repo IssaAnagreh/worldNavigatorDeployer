10821d2fcd42e2cfc32f569236164493bdc8827d
IssaAnagreh <isa_anagreh@hotmail.com>
2020-07-03 22:42:55 +0300

    check of the map works fine

--- src/main/java/com/worldNavigator/MapFactory.java
+++ src/main/java/com/worldNavigator/MapFactory.java
@@ -1,9 +1,6 @@
 package com.worldNavigator;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,17 +11,26 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class MapFactory {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MenuServer")
+public class MapFactory extends HttpServlet {
   private String name = "empty";
   public List<Room> rooms = new ArrayList<>();
   public int endTime;
   private int room_counter = 0;
-  public final String mapName;
+  public String mapName;
   public Map<String, Object> contents;
   public String location;
   public String orientation;
   public int roomIndex;
   public JSONArray jsonRooms;
+
+  private static final long serialVersionUID = 1L;
 
   private JSONObject castToJSONObject(Object o) {
     return (JSONObject) o;
@@ -32,6 +38,14 @@ public class MapFactory {
 
   private JSONArray castToJSONArray(Object o) {
     return (JSONArray) o;
+  }
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public MapFactory() {
+    super();
+    // TODO Auto-generated constructor stub
   }
 
   @SuppressWarnings("unchecked")
@@ -107,4 +121,35 @@ public class MapFactory {
   public String toString() {
     return name;
   }
+
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    // Step 1: set content type
+    response.setContentType("text/html");
+
+    // Step 2: get the printwriter
+    PrintWriter out = response.getWriter();
+
+    // Step 3: generate the HTML content
+    out.println("<html><body>");
+
+    out.println("Map Name: "
+            + this.mapName);
+
+    out.println("</body></html>");
+
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
+
 }
