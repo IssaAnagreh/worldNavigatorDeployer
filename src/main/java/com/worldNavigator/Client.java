66cd824a06651c21d0852b29a02c994e0014bf5f
IssaAnagreh <isa_anagreh@hotmail.com>
2020-07-03 23:25:13 +0300

    check of the map works fine

--- src/main/java/com/worldNavigator/Client.java
+++ src/main/java/com/worldNavigator/Client.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class StudentServlet
  */
-@WebServlet("/client")
+@WebServlet("/Client")
 public class Client extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
